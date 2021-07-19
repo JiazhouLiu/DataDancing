ec6a5b0ebc66f8b867a7024d233f0c4b54837ae2
Joe Liu <jiazhou.liu@monash.edu>
2021-07-19 18:59:26 +1000

    Transformed from HDRP to normal render pipeline

---
+++ Assets/Script/Utilities/CustomButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(DashboardController_UserStudy))]
+public class CustomButton : Editor
+{
+    //public override void OnInspectorGUI()
+    //{
+    //    DrawDefaultInspector();
+
+    //    DashboardController_UserStudy myScript = (DashboardController_UserStudy)target;
+    //    if (GUILayout.Button("Get Shoulder Position"))
+    //    {
+    //        myScript.GetShoulderPosition();
+    //    }
+
+    //    if (GUILayout.Button("Get Arm Length"))
+    //    {
+    //        myScript.GetArmLength();
+    //    }
+    //}
+
+}
